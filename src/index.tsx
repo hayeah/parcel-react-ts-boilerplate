00cd55269292a74c45695e5a3c2d60dbfc059ea5
Howard Yeh <howard@metacircus.com>
2018-02-09 13:39:15 +0800

    stuff

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,16 @@ import "./index.css"
 import * as React from "react"
 import { render } from "react-dom"
 
-const view = (
-  <div>
-    <h1>
-      Hello React TypeScript <span className="fa fa-heart" />
-    </h1>
+import { App } from "./views/App"
 
-    <h2>
-      VERSION: {VERSION}
-    </h2>
-
-    <h2>
-      RELEASE: {RELEASE}
-    </h2>
-  </div>
-)
+function renderApp() {
+  render(<App />, document.getElementById("root"))
+}
 
 window.addEventListener("load", () => {
-  render(view, document.getElementById("root"))
+  renderApp()
 })
+
+if (module.hot) {
+  module.hot.accept(renderApp)
+}
---
+++ src/views/App.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        Hello React TypeScript <span className="fa fa-heart" />
+      </h1>
+
+      {/* <h2>
+      VERSION: {VERSION}
+    </h2>
+
+    <h2>
+      RELEASE: {RELEASE}
+    </h2> */}
+    </div>
+  )
+}
